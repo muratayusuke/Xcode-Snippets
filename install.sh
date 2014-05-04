1c8b37607f151d017f44f4491286264973df89e2
muratayusuke <info@muratayusuke.com>
2014-05-04 19:55:04 +0900

    add first snippet

--- install.sh
+++ install.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-SNIPPETS_DIR="~/Library/Developer/Xcode/UserData/CodeSnippets"
+SNIPPETS_DIR=~/Library/Developer/Xcode/UserData/CodeSnippets
 DST_DIR=`dirname $0`/snippets
+
+# get absolute path for symlink destination
+pushd $DST_DIR > /dev/null
+DST_DIR=`pwd`
+popd > /dev/null
 
 if [ -d $SNIPPETS_DIR ]; then
     mv $SNIPPETS_DIR{,.bk}
